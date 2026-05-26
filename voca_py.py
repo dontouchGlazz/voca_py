5f7a3f5b2008d5dbadef3cbb645bb9e80e2fa27c
dontouchGlazz <grace.qx.yin@gmail.com>
2026-05-26 13:09:18 -0400

    initalized .csv and .py

---
+++ voca_py.py
@@ -0,0 +1,14 @@
+#Chinese character flashcard pratice
+
+import random
+
+#json
+#tkinter
+
+#flashcard mode
+#--->read from json
+#--->randomly scramble list
+#--->then go through this list, 0, 1, 2...
+
+#recording mode
+#--->enter new word and add to json
